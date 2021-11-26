e30c70d89ca852f8bd8bb5b96040ae4539aa3cce
deji <odedejie@gmail.com>
2021-11-26 15:00:27 +0100

    sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './page/landing-page/landing-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: LandingPageComponent},
   {
     path: '',
     loadChildren: () => import('./page/page.module').then(m => m.PageModule)
-  }
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
