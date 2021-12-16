c22fa97142d059584f932841e2a9c8415fc943a6
deji <odedejie@gmail.com>
2021-12-16 15:03:48 +0100

    g

--- src/app/page/page-routing.module.ts
+++ src/app/page/page-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PageComponent } from './page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PageComponent,
     children: [
-      {path: '', component: LandingPageComponent}
+      {path: '', component: LandingPageComponent},
+      {path: 'dashboard', component: DashboardComponent}
     ]
   }
 ];
