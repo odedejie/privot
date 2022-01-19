79173e3b2760b13dbda5820b732c97cb809c6c57
deji <odedejie@gmail.com>
2022-01-19 23:19:58 +0100

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PrivotDashboardComponent } from './privot-dashboard/privot-dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    PrivotDashboardComponent
   ],
   imports: [
     BrowserModule,
